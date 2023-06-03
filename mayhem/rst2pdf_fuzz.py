154eefc0895b244f3334fdfe5f91cd35e39a7e95
jake <jake@ubuntudev.myguest.virtualbox.org>
2023-06-03 19:51:40 -0400

    More test cases

--- mayhem/rst2pdf_fuzz.py
+++ mayhem/rst2pdf_fuzz.py
@@ -4,61 +4,30 @@ import sys
 
 with atheris.instrument_imports(include=['rst2pdf']):
     from rst2pdf.createpdf import RstToPdf
-
-    # tylesheets: list = [],
-    # language: str = 'en_US',
-    # header: Any = None,
-    # footer: Any = None,
-    # inlinelinks: bool = False,
-    # breaklevel: int = 1,
-    # font_path: list = [],
-    # style_path: list = [],
-    # fit_mode: str = 'shrink',
-    # background_fit_mode: str = 'center',
-    # sphinx: bool = False,
-    # smarty: str = '0',
-    # baseurl: Any = None,
-    # repeat_table_rows: bool = False,
-    # footnote_backlinks: bool = True,
-    # inline_footnotes: bool = False,
-    # real_footnotes: bool = False,
-    # def_dpi: int = 300,
-    # show_frame: bool = False,
-    # highlightlang: str = 'python',
-    # basedir: str = os.getcwd(),
-    # splittables: bool = False,
-    # blank_first_page: bool = False,
-    # first_page_on_right: bool = False,
-    # breakside: str = 'odd',
-    # custom_cover: str = 'cover.tmpl',
-    # floating_images: bool = False,
-    # numbered_links: bool = False,
-    # section_header_depth: int = 2,
-    # toc_depth: int = 0,
-    # raw_html: bool = False,
-    # strip_elements_with_classes: list = [],
-    # record_dependencies: Any = None
-
-
+    from reportlab.lib.styles import getSampleStyleSheet
 
 
 def TestOneInput(data):
     global r2p
 
     fdp = atheris.FuzzedDataProvider(data)
-    #
-    consumed_bool = fdp.ConsumeBool()
-    try:
-        # RstToPdf(stylesheets=[]).createPdf(
-        #     text=consumed_bytes,
-        #     output='/dev/null',
-        #     source_path=None,
-        # )
-        r2p.floating_images = consumed_bool
 
+    consumed_bool = fdp.ConsumeBool()
+    consumed_sep = fdp.ConsumeString(1)
+    consumed_str = fdp.ConsumeString(fdp.remaining_bytes())
+    styles = getSampleStyleSheet()
+    try:
+        if consumed_bool:
+            r2p.style_language('en')
+        else:
+            r2p.style_language('cn')
+        r2p.author_separator(consumed_sep)
+        style = styles['Normal']
+        r2p.PreformattedFit(consumed_str, style)
+    except ValueError:
+        return
     except Exception:
         raise
-    return
 
 def main():
     atheris.Setup(sys.argv, TestOneInput)
