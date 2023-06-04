a5ef007c41fc775a8fd4381525587d92335107f1
jake <jake@ubuntudev.myguest.virtualbox.org>
2023-06-03 21:12:05 -0400

    More speed?

--- mayhem/rst2pdf_fuzz.py
+++ mayhem/rst2pdf_fuzz.py
@@ -7,6 +7,7 @@ import sys
 
 with atheris.instrument_imports(include=['rst2pdf']):
     from rst2pdf.createpdf import RstToPdf
+    from reportlab.lib.styles import getSampleStyleSheet
 
 def TestOneInput(data):
     global r2p
@@ -14,13 +15,17 @@ def TestOneInput(data):
     ran = fdp.ConsumeIntInRange(0, 20)
     consumed_bytes = fdp.ConsumeString(fdp.ConsumeIntInRange(0, fdp.remaining_bytes()))
     try:
-        if ran % 2 == 0:
+        if ran % 3 == 0:
             r2p.createPdf(
                 text=consumed_bytes,
                 output='/dev/null',
                 source_path=None,
                 debugLinesPdf=False
             )
+        else:
+            styles = getSampleStyleSheet()
+            style = styles['Normal']
+            r2p.PreformattedFit(consumed_bytes, style)
     except Exception:
         raise
 
